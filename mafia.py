1fc5bd838c916a7b4881f512f6b23ea5d2548f9e
sadrasabouri <sabouri.sadra@gmail.com>
2020-08-09 17:41:48 +0430

    add : html templates and css files added.

--- mafia.py
+++ mafia.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template("index.html",
+                            image_name="Don",
+                            role_name="Don",
+                            is_farsi=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", \
