4ffb323c4e646575e1ac7ab463b6b5cfbe520333
sadrasabouri <sabouri.sadra@gmail.com>
2020-08-09 19:22:01 +0430

    add : mafia_params.py added.

---
+++ mafia_params.py
@@ -0,0 +1,16 @@
+ordered_roles = ["Resident",
+                 "Don",
+                 "Resident",
+                 "Mafia",
+                 "Doctor",
+                 "Detector",
+                 "Mafia",
+                 "Resident",
+                 "Rebel"]
+
+nRoles = {"Don" : 1,
+          "Resident" : 4,
+          "Mafia" : 2,
+          "Doctor" : 2,
+          "Rebel" : 1,
+          "Detective" : 1}
