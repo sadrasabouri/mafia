0df5b56e66d65d5419ed2bfb82f56664cd0622bf
sadrasabouri <sabouri.sadra@gmail.com>
2020-08-11 14:11:58 +0430

    fix : typo fixed.

--- mafia_params.py
+++ mafia_params.py
@@ -3,7 +3,7 @@ ordered_roles = ["Resident",
                  "Resident",
                  "Mafia",
                  "Doctor",
-                 "Detector",
+                 "Detective",
                  "Mafia",
                  "Resident",
                  "Rebel",
@@ -24,7 +24,7 @@ descriptions = {"Don" : "Don is the boss of the mafia group. At night mode Don d
            "Resident" : "Resident is the typical player of the game. he/she has no power but to blame mafia in order to remove them from the game in day mode.",
            "Mafia" : "Mafia is the simple participant of the mafia team. Mafia gets up at night and try to decide which one of the players they want to kill.",
            "Doctor": "Doctor gets up after mafia team and tries to rescue a person (or two in the first night) from mafia's shot.",
-           "Detector" : "Detector gets up after Doctor and tries to ask GOD if someone is good (Resident, Doctor, Rebel, Anti-attack)."
+           "Detective" : "Detective gets up after Doctor and tries to ask GOD if someone is good (Resident, Doctor, Rebel, Anti-attack)."
                         "or bad(Mafia), but his/her first attempt to ask from Don may be answered incorrect by GOD.",
            "Rebel" : "Rebel gets up at night mode and kills a person.if the victim was chosen from residents, Rebel (him/her)self may die.",
            "Anti-attack" : "Anti-attack doesn't hurt from night shots. he/she won't die through night mode."}
@@ -33,6 +33,6 @@ role2fa = {"Don" : "دن",
            "Resident" : "شهروند",
            "Mafia" : "مافیا",
            "Doctor": "دکتر",
-           "Detector" : "کارآگاه",
+           "Detective" : "کارآگاه",
            "Rebel" : "شورشی",
            "Anti-attack" : "ضد ضربه"}
