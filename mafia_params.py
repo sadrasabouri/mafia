a81dae1437dd63f1e2b8cf8dab634267c4056e6b
sadrasabouri <sabouri.sadra@gmail.com>
2020-08-11 11:02:17 +0430

    edit : minor edits in mafia_params.py

--- mafia_params.py
+++ mafia_params.py
@@ -9,6 +9,7 @@ ordered_roles = ["Resident",
                  "Rebel",
                  "Anti-attck",
                  "Mafia",
+                 "Resident",
                  "Resident"]
 
 nRoles = {"Don" : 1,
@@ -19,14 +20,14 @@ nRoles = {"Don" : 1,
           "Detective" : 1,
           "Anti-attack" : 1}
 
-descriptions = {"Don" : "Don is the boss of the mafia group. At night mode he'll decide whom to be killed from mafia team.",
-           "Resident" : "Resident is the most simple player in the mafia game. he/she has no power but to blame mafia in order to remove them from game in day mode.",
-           "Mafia" : "Mafia is the simple mafia player. they'll get up at night and try to decide which one of the peoples they want to kill.",
-           "Doctor": "Doctor gets up after mafia team and try to rescue a person (or two in first night) from mafia's attack.",
-           "Detector" : "Detector gets up after Doctor and try to ask GOD if someone is good (Resident, Doctor, Rebel, Anti-attack) <br>"
+descriptions = {"Don" : "Don is the boss of the mafia group. At night mode Don decides whom to be killed from the mafia team.",
+           "Resident" : "Resident is the typical player of the game. he/she has no power but to blame mafia in order to remove them from the game in day mode.",
+           "Mafia" : "Mafia is the simple participant of the mafia team. Mafia gets up at night and try to decide which one of the players they want to kill.",
+           "Doctor": "Doctor gets up after mafia team and tries to rescue a person (or two in the first night) from mafia's shot.",
+           "Detector" : "Detector gets up after Doctor and tries to ask GOD if someone is good (Resident, Doctor, Rebel, Anti-attack)."
                         "or bad(Mafia), but his/her first attempt to ask from Don may be answered incorrect by GOD.",
-           "Rebel" : "Rebel gets up at night mode and kill a person.if the victim was chosen from residents Rebel (him/her)self may die",
-           "Anti-attack" : "Anti-attack won't hurt from night shoots. he/she won't die through night mode."}
+           "Rebel" : "Rebel gets up at night mode and kills a person.if the victim was chosen from residents, Rebel (him/her)self may die.",
+           "Anti-attack" : "Anti-attack doesn't hurt from night shots. he/she won't die through night mode."}
 
 role2fa = {"Don" : "دن",
            "Resident" : "شهروند",
