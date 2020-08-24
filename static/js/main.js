e699ed75a87789766c089aac5c79fddde883aa8d
sadrasabouri <sabouri.sadra@gmail.com>
2020-08-24 18:36:46 +0430

    change : minor changes in UI.

--- static/js/main.js
+++ static/js/main.js
@@ -5,17 +5,17 @@ function start() {
     var elem = document.getElementById("myBar");
     var width = 0;
     var time_from_start = 0;
-    var id = setInterval(frame, 1000);
+    var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
       } else {
-        width += 100 / 45;
-        time_from_start ++;
+        width += 1 / 45;
+        time_from_start = 45 * width / 100;
         elem.style.width = width + "%";
-        elem.innerHTML = time_from_start;
-        elem.style.backgroundColor = "rgb(" + time_from_start * 5 +", 138, 223)"; 
+        elem.innerHTML = time_from_start.toFixed(2);
+        elem.style.backgroundColor = "rgb(" + time_from_start * 6 +", 50, 50)"; 
       }
     }
   }
