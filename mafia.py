45dd4375234d1091c2c15d0f8297a133d4a1042c
sadrasabouri <sabouri.sadra@gmail.com>
2020-09-10 12:32:16 +0430

    add : role2team passed to god page.

--- mafia.py
+++ mafia.py
@@ -106,7 +106,7 @@ def GOD_PAGE():
             return render_template("404.html", is_farsi=True)
     return render_template("GOD.html", ip2role_index_name=ip2role_index_name,
                            prompt_message=msg, roles={role:roles.count(role) for role in set(roles)},
-                           comments=comments_ordered)
+                           comments=comments_ordered, role2team=role2team)
 
  
 @app.errorhandler(404) 
